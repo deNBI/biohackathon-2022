01c5af51df35ddfbc538638b95ef94a09ea4bdde
Josh Moore <josh@openmicroscopy.org>
2022-12-14 12:08:52 +0100

    LinkML Crosswalk update
    - Make use of schema in #8
    - Generate simple JSON-LD as the output
    - Hide other outputs behind `logging.debug`

--- projects/bioschemas4nfdi/linkml-crosswalk/translate.py
+++ projects/bioschemas4nfdi/linkml-crosswalk/translate.py
@@ -8,14 +8,16 @@ from linkml_runtime.utils.schemaview import SchemaView
 
 
 from rdflib import Graph, URIRef
+from json import dumps
 
 
 @click.command()
 @click.argument("model", type=click.Path(exists=True))
 @click.argument("data", type=click.Path(exists=True))
-@click.argument("mapping", type=str)
-@click.option("-l", "--log-level", type=LogLevel(), default=logging.INFO)
-def dump(model, data, mapping, log_level):
+@click.option("-p", "--prefix", type=str, default="bioschemas")
+@click.option("-i", "--indent", type=int, default=4)
+@click.option("-l", "--log-level", type=LogLevel(), default=logging.ERROR)
+def dump(model, data, prefix, indent, log_level):
 
     logging.basicConfig(level=log_level)
 
@@ -26,8 +28,11 @@ def dump(model, data, mapping, log_level):
     for ckey, cvalue in sv.schema.classes.items():
         for skey, svalue in cvalue.slot_usage.items():
             for mapping in svalue.exact_mappings:
-                print(ckey, skey, "->", mapping)
+                logging.debug(ckey, skey, "->", mapping)
                 uri = sv.get_uri(svalue)
+                if not mapping.startswith(prefix):
+                    logging.info("Skipping prefix: %s!=%s", mapping, prefix)
+                    continue
                 if ":" in uri:
                     first, second = uri.split(":")
                     uri = f"{ns[first]}{second}"
@@ -38,15 +43,26 @@ def dump(model, data, mapping, log_level):
 
     graph = Graph().parse(data)
     output = Graph()
-    print("INPUT")
     for s, p, o in graph:
-        print(s, p, o)
+        logging.debug("INPUT:" + "%s"*3, s, p, o)
         lookup = mappings.get(str(p))
         if lookup:
             output.add((s, URIRef(lookup), o))
-    print("OUTPUT")
+
+    jsonld = {
+        "@context": "http://schema.org",
+        "@type": "Dataset",
+    }
     for s, p, o in output:
-        print(s, p, o)
+        logging.debug("OUTPUT:" + "%s"*3, s, p, o)
+
+        # Without json-ld framing, it's unclear how
+        # best to go from these triples to a desired
+        # block of JSON-LD.
+        key = str(p).split("/")[-1]
+        jsonld[key] = str(o)
+
+    print(dumps(jsonld, indent=indent, sort_keys=True))
 
 
 if __name__ == "__main__":
